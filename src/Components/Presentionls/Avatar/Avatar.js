c5fc26fc4ba17a354fed4e66a79edaff5642232c
Kamil <kamil.dziolak@gmail.com>
2019-02-22 14:16:33 +0100

    Updates

--- src/Components/Presentionls/Avatar/Avatar.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { StyledAvatar } from './AvatarStyles'
-
-
-const Avatar = (props) => <StyledAvatar size={props.size} avatarImg={props.avatarImg} />
-
-export default Avatar;
--- src/Components/Presentionls/CardPanel/CardHeader/Text/Text.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { StyledText, StyledSpan } from './TextStyles'
-
-const Text = ({ text, notyfications }) => <StyledText>{text} {notyfications ? '(' : ''}{notyfications ? <StyledSpan>{notyfications}</StyledSpan> : ''}{notyfications ? ')' : ''}</StyledText>
-
-
-export default Text;    
